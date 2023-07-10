5b1fd2d6312302b75ee730a3e26ece7dffd00373
Nezir Bayhan <130616437+crinklyfire@users.noreply.github.com>
2023-07-10 15:40:57 +0300

    Add files via upload

---
+++ Hafta 1/src/Exercises/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Seri eleman sayısı: ");
+        int R = input.nextInt();
+
+        int num1 = 0, num2 = 1;
+        int counter = 0;
+
+        // Iterate till counter is R
+        while (counter < R){
+
+            //Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
+    }
+}
